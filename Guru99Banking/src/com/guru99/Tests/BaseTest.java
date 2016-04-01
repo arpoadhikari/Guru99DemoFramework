71845644da5cbbb3168bdada937d1099aa01b39e
Arpo <arpoadhikari@gmail.com>
2016-04-01 22:08:12 +0530

    Fixed

--- Guru99Banking/src/com/guru99/Tests/BaseTest.java
+++ Guru99Banking/src/com/guru99/Tests/BaseTest.java
@@ -47,8 +47,6 @@ public class BaseTest {
 	@AfterMethod
 	public void endTestMethod(Method method) {
 		Log.endTestCase(testMethodName);
-		if (!driver.getTitle().equalsIgnoreCase("Guru99 Bank Home Page")) {
-			
-		}
+
 	}
 }
